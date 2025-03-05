7b8bf3c64f8875f88850eae72fd11048b846ad6c
rath-samrith-webdev <rathsamrith.webdev@gmail.com>
2025-03-05 15:28:27 +0700

    type-variable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int age=30; //This type take the whole number;
+        age=35; // Re assigning
+        //declare multi-variables in a single lines
+        /*
+        * @Note that this on work with the same type
+        * int age =49,speed=10,life=10
+        */
+        int myAge= age;
+        System.out.println(myAge);
     }
 }
