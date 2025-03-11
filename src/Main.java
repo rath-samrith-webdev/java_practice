7f0550e21706869a1d5449a05882ee10cba9aef9
rath-samrith-webdev <rathsamrith.webdev@gmail.com>
2025-03-11 15:11:04 +0700

    constant

--- src/Main.java
+++ src/Main.java
@@ -60,5 +60,81 @@ public class Main {
          * */
         int[][] matrix =new int[2][3];
         System.out.println(Arrays.deepToString(matrix));
+
+        /**
+         * @NOTE
+         *      To declare a constant you need to use the keyword
+         * @final which mean finalize
+         * @e.g
+         *      final byte number = 1
+         *
+         * */
+        final byte number=1;
+        /**
+         * @Assign
+         *      If you try to assign the new value to the final
+         *      variable it will show the compilation error
+         *
+         * @Cannot assign a value to final variable '<variable_name>'
+         * */
+        System.out.println(number);
+
+        /**
+         * @Arithmethic-Expression
+         * @Arithmethic-Expressions consists of
+         * @+ Addition
+         * @- Subtraction
+         * @/ Division
+         * @% Modular is to get the remains of Division
+         * @* Multiplication
+         * */
+        int ten = 10+1;
+        System.out.println("@Arithmethic-Expression "+ ten);
+
+
+        /**
+         * @NOTE
+         * @Division of a whole numbers result is a whole number.
+         *
+         * @NOTE
+         * To get the floating numbers you need to case it <Operate>
+         * @ex
+         * int ten = 10/3 the result will be a whole number 3
+         * <type> ten = (type)10/(type)3
+         * */
+        int results = 10/3;
+        System.out.println(results);
+        double resultsD = (double)10/(double) 3;
+        System.out.println(resultsD);
+
+        /**
+         * @NOTE
+         * normally you would
+         * use x++
+         * THE + Operator has different uses
+         * @1
+         * if you use ++x x will be incremented by 1
+         * the result is 2
+         * @ex
+         * x=1
+         * y=++x this will first increment x by 1 and then assign it to y
+         * @2
+         * if you use x++ x will first copied and assigned to the variable (if you declare if not there's no problem)
+         * @ex
+         * x = 1
+         * y = x++ this will first set y to 1 and the increment x
+         *
+         * @NOTE
+         * In case you want to use it with number different from 1
+         * @eg
+         * x+=2
+         * x-=2
+         * x/=2
+         * x*=2
+         * this above is called compound assignment operator
+         * */
+        int x = 1;
+        x++;
+        System.out.println("compound assignment operator "+x);
     }
 }
